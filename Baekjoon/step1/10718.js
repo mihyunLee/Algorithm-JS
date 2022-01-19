d22166c006e549cb5d3d0b8f5038592d36d99caa
mihyun <mihy621@gmail.com>
2022-01-19 21:33:52 +0900

    10718: We love krii, Using let keyword

--- Baekjoon/step1/10718.js
+++ Baekjoon/step1/10718.js
@@ -3,19 +3,19 @@ console.log("강한친구 대한육군");
 console.log("강한친구 대한육군");
 
 // 2. for문 (108ms)
-for (i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
   console.log("강한친구 대한육군");
 }
 
 // 3. do..while문 (108ms)
-i = 0;
+let i = 0;
 do {
   i += 1;
   console.log("강한친구 대한육군");
 } while (i < 2);
 
 // 4. while문 (112ms)
-i = 0;
+i = 0; // 3번에서 let으로 이미 선언했으므로 let 사용하지 않기
 while (i < 2) {
   i += 1;
   console.log("강한친구 대한육군");
