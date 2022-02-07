6fdabf98965e90e990398d4321fb0f2e631bf288
mihyun <mihy621@gmail.com>
2022-02-07 11:02:58 +0900

    9498: 시험 성적

---
+++ Baekjoon/step2/9498.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `100`;
+
+const score = input;
+
+// switch-case
+switch (parseInt(score / 10)) {
+  case 10:
+  case 9:
+    console.log("A");
+    break;
+  case 8:
+    console.log("B");
+    break;
+  case 7:
+    console.log("C");
+    break;
+  case 6:
+    console.log("D");
+    break;
+  default:
+    console.log("F");
+}
+
+// if-else
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else if (score >= 70 && score <= 79) {
+  console.log("C");
+} else if (score >= 60 && score <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
