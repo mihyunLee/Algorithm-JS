1ef1c0206f1ea5dfb6fb7b92ed280af41f1da83f
mihyun <mihy621@gmail.com>
2022-02-23 20:39:50 +0900

    2741: N 찍기

---
+++ Baekjoon/step3/2741.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const stdin =
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `5`;
+
+let answer = "";
+
+for (let i = 1; i <= stdin; i++) {
+  answer += i + "\n";
+}
+console.log(answer);
