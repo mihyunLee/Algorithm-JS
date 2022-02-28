162c48ec25736510436391ef6894f6e0037f833e
mihyun <mihy621@gmail.com>
2022-02-28 16:19:33 +0900

    2438: 별 찍기 - 1

---
+++ Baekjoon/step3/2438.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const stdin = Number(
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `5`
+);
+
+let answer = "";
+
+for (let i = 1; i <= stdin; i++) {
+  answer += "*";
+  console.log(answer);
+}
