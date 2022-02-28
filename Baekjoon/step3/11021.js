d9b3d11b7e9afe1793f6cdf1f1065867e87b0d10
mihyun <mihy621@gmail.com>
2022-02-28 15:56:16 +0900

    11021: A+B -7

---
+++ Baekjoon/step3/11021.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `5
+    1 1
+    2 3
+    3 4
+    9 8
+    5 2`
+).split("\n");
+
+const T = Number(stdin[0]);
+let answer = "";
+
+for (let i = 1; i <= T; i++) {
+  let input = stdin[i].split(" ");
+  answer += `Case #${i}: ${Number(input[0]) + Number(input[1])}\n`;
+}
+
+console.log(answer);
