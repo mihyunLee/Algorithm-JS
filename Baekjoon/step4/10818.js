4b29792ad10caab27a87dcea2bcdca2f0a605f49
mihyun <mihy621@gmail.com>
2022-04-28 01:00:11 +0900

    10818: fix - min value is undefined

--- Baekjoon/step4/10818.js
+++ Baekjoon/step4/10818.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-
-const input =
+const input = (
   process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin").toString().split("\n")
+    ? require("fs").readFileSync("/dev/stdin").toString()
     : `5
-    20 10 35 30 7`;
+20 10 35 30 7`
+).split("\n");
 
 let count = Number(input[0]);
 let numbers = input[1].split(" ").map(Number);
 
-let min,
-  max = numbers[0];
+let min = numbers[0];
+let max = numbers[0];
 
 for (let i = 1; i < count; i++) {
   if (max < numbers[i]) {
     max = numbers[i];
   }
+
   if (min > numbers[i]) {
     min = numbers[i];
   }
