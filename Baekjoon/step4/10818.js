9d978f0c2bec4ebe2a0c965bf37291046f67adfe
mihyun <mihy621@gmail.com>
2022-04-28 18:51:59 +0900

    10818: 최소, 최대 (using sort)

--- Baekjoon/step4/10818.js
+++ Baekjoon/step4/10818.js
@@ -2,23 +2,12 @@ const input = (
   process.platform === "linux"
     ? require("fs").readFileSync("/dev/stdin").toString()
     : `5
-20 10 35 30 7`
+    20 10 35 30 7`
 ).split("\n");
 
 let count = Number(input[0]);
 let numbers = input[1].split(" ").map(Number);
 
-let min = numbers[0];
-let max = numbers[0];
+numbers.sort((a, b) => a - b);
 
-for (let i = 1; i < count; i++) {
-  if (max < numbers[i]) {
-    max = numbers[i];
-  }
-
-  if (min > numbers[i]) {
-    min = numbers[i];
-  }
-}
-
-console.log(`${min} ${max}`);
+console.log(`${numbers[0]} ${numbers[count - 1]}`);
