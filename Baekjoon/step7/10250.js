261079937de1ba7352ada1ba7569d4308c37422e
mihyunLee <mihy621@gmail.com>
2022-06-22 00:26:42 +0900

    refactor: 10250 - ACM 호텔

--- Baekjoon/step7/10250.js
+++ Baekjoon/step7/10250.js
@@ -9,12 +9,10 @@ const input = (
 const T = input[0];
 
 for (let i = 1; i <= T; i++) {
-  let Y = 0,
-    X = 0;
   let [H, W, N] = input[i].trim().split(" ").map(Number);
 
-  Y = N % H === 0 ? H : N % H;
-  X = N / H < 10 ? `0${Math.ceil(N / H)}` : Math.ceil(N / H);
+  let Y = N % H === 0 ? H : N % H;
+  let X = Math.ceil(N / H);
 
-  console.log(`${Y}${X}`);
+  X < 10 ? console.log(`${Y}0${X}`) : console.log(`${Y}${X}`);
 }
