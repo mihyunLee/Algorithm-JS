daa4fb54153251d8f974dd31f6314fb29a7ac0c7
mihyunLee <mihy621@gmail.com>
2022-07-05 22:36:51 +0900

    refactor: 제일 작은 수 제거하기 - Math.min() 사용

--- Programmers/Level1/제일작은수제거하기.js
+++ Programmers/Level1/제일작은수제거하기.js
@@ -1,9 +1,4 @@
 function solution(arr) {
-  let min = arr[0];
-
-  arr.forEach((item) => (min = min > item ? item : min));
-
-  const filtered = arr.filter((item) => item !== min);
-
-  return filtered.length > 0 ? filtered : [-1];
+  let min = Math.min(...arr);
+  return arr.length !== 1 ? arr.filter((item) => item !== min) : [-1];
 }
