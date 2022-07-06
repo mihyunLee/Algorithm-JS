59c2bce4664bb6ca95cc53b300742e9c96ee2828
mihyunLee <mihy621@gmail.com>
2022-07-06 23:07:34 +0900

    refactor: 9020 - 문제 풀이 순서를 변경하여 메모리 줄이기

--- Baekjoon/step8/9020.js
+++ Baekjoon/step8/9020.js
@@ -9,32 +9,33 @@ const input = (
   .split("\n")
   .map(Number);
 
-let i = 1;
+// 에라토스테네스의 체를 이용하여 2부터 10,000까지 소수 구하기
+const n = 10000;
+const isPrimeNumber = new Array(n + 1).fill(true);
+isPrimeNumber[0] = isPrimeNumber[1] = false;
 
-while (i <= input[0]) {
-  let n = input[i];
-
-  const isPrimeNumber = new Array(n + 1).fill(true);
-  isPrimeNumber[0] = isPrimeNumber[1] = false;
-
-  for (let i = 2; i <= Math.ceil(Math.sqrt(n)); i++) {
-    if (!isPrimeNumber[i]) {
-      continue;
-    }
-
-    for (let j = i * 2; j <= n * 2; j += i) {
-      isPrimeNumber[j] = false;
-    }
+for (let i = 2; i <= Math.ceil(Math.sqrt(n)); i++) {
+  if (!isPrimeNumber[i]) {
+    continue;
   }
 
-  const primeNumber = [];
-
-  for (let i = 2; i < n; i++) {
-    if (isPrimeNumber[i]) {
-      primeNumber.push(i);
-    }
+  for (let j = i * 2; j <= n * 2; j += i) {
+    isPrimeNumber[j] = false;
   }
+}
 
+const primeNumber = [];
+
+for (let i = 2; i < n; i++) {
+  if (isPrimeNumber[i]) {
+    primeNumber.push(i);
+  }
+}
+
+// 테스트 케이스별로 골드바흐 파티션 구하기
+
+for (let i = 1; i <= input[0]; i++) {
+  const n = input[i];
   const partition = [];
 
   for (let i = 0; primeNumber[i] <= n / 2; i++) {
@@ -47,6 +48,4 @@ while (i <= input[0]) {
   const answer = partition.pop();
 
   console.log(`${answer[0]} ${answer[1]}`);
-
-  i++;
 }
