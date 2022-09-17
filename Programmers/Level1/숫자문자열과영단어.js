b20bf684f0eab5ef582dce0ea5eee451eaad9a22
mihyunLee <mihy621@gmail.com>
2022-09-17 22:39:56 +0900

    숫자 문자열과 영단어 - level 1

---
+++ Programmers/Level1/숫자문자열과영단어.js
@@ -0,0 +1,22 @@
+function solution(s) {
+  let numArr = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  var answer = s;
+
+  for (let i = 0; i < numArr.length; i++) {
+    let arr = answer.split(numArr[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
